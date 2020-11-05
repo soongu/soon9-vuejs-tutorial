c0867fd334e7f14fadc89520696ebbb5838bc61b
Soon9 <naong526@naver.com>
2020-11-05 18:32:18 +0900

    [20.11.05] #2. 컴포넌트 생성

---
+++ stage3/components/MessageList.js
@@ -0,0 +1,26 @@
+import MessageListItem from "./MessageListItem.js";
+export default {
+    name: 'MessageList',
+    template: `
+    <ul>
+        <message-list-item 
+            v-for="item in items" 
+            :item="item"
+            :key="item.id"
+            @delete="deleteMessage(item)" />
+    </ul>`,
+    props: {
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    components: {
+        MessageListItem
+    },
+    methods: {
+        deleteMessage (message) {
+            this.$emit('delete', message)
+        }
+    }
+}
